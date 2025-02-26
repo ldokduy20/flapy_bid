5b310ff33a427026e3c170f109a8d99896dfdb77
Duy <khangduyluongdo@gmail.com>
2025-02-26 22:53:30 +0700

    Use rectangles for drawing pipes and bird

--- src/bird.zig
+++ src/bird.zig
@@ -3,22 +3,27 @@ const config = @import("config.zig");
 
 pub const Bird = struct {
     // x and y position is in the **center**.
-    x: i32,
-    y: i32,
-    vel_y: i32,
-    grav: i32,
+    x: f32,
+    y: f32,
+    vel_y: f32,
+    grav: f32,
 
     w: i32,
     h: i32,
 
     tex: *const rl.Texture2D,
 
-    pub fn new(x: i32, y: i32, tex: *const rl.Texture2D) Bird {
+    pub fn new(x: f32, y: f32, tex: *const rl.Texture2D) Bird {
         return .{ .x = x, .y = y, .vel_y = 0, .grav = config.BIRD_GRAVITY, .tex = tex, .w = tex.width, .h = tex.height };
     }
 
     pub fn draw(b: *const Bird) void {
-        rl.drawTexture(b.tex.*, b.x - @divTrunc(b.w, 2), b.y - @divTrunc(b.h, 2), rl.Color.white);
+        // rl.drawTexture(b.tex.*, b.x - @divTrunc(b.w, 2), b.y - @divTrunc(b.h, 2), rl.Color.white);
+        rl.drawTextureV(
+            b.tex.*,
+            rl.Vector2{ .x = b.x - (@as(f32, @floatFromInt(b.w)) / 2.0), .y = b.y - (@as(f32, @floatFromInt(b.h)) / 2.0) },
+            rl.Color.white,
+        );
     }
 
     pub fn update(b: *Bird) void {
--- src/pipe.zig
+++ src/pipe.zig
@@ -1,4 +1,3 @@
-// zig fmt: off
 const rl = @import("raylib");
 const GAP = @import("config.zig").GAP;
 
@@ -8,20 +7,28 @@ pub const Pipe = struct {
     vel_x: i32,
     tex: *const rl.Texture2D,
 
+    top_rect: rl.Rectangle,
+    bot_rect: rl.Rectangle,
+
     pub fn new(x: i32, y: i32, vel_x: i32, tex: *const rl.Texture2D) Pipe {
-        return .{
-            .x = x,
-            .y = y,
-            .vel_x = vel_x,
-            .tex = tex,
-        };
+        return .{ .x = x, .y = y, .vel_x = vel_x, .tex = tex, .top_rect = rl.Rectangle{
+            .x = @floatFromInt((x - @divTrunc(tex.width, 2))),
+            .y = @floatFromInt((y - @divTrunc(GAP, 2)) - tex.height),
+            .width = @floatFromInt(tex.width),
+            .height = @floatFromInt(tex.height),
+        }, .bot_rect = rl.Rectangle{
+            .x = @floatFromInt((x - @divTrunc(tex.width, 2))),
+            .y = @floatFromInt(y + @divTrunc(GAP, 2)),
+            .width = @floatFromInt(tex.width),
+            .height = @floatFromInt(tex.height),
+        } };
     }
 
     pub fn draw(p: *const Pipe) void {
         // rl.drawTextureEx(
         //     p.tex.*,
-        //     rl.Vector2{ 
-        //         .x = @floatFromInt((p.x - @divTrunc(p.tex.width, 2))), 
+        //     rl.Vector2{
+        //         .x = @floatFromInt((p.x - @divTrunc(p.tex.width, 2))),
         //         .y = @floatFromInt((p.y - @divTrunc(GAP, 2)))
         //     },
         //     180.0,
@@ -35,30 +42,42 @@ pub const Pipe = struct {
                 .y = 0.0,
                 .width = @floatFromInt(p.tex.width),
                 .height = @floatFromInt(-p.tex.height),
-            }, 
-            rl.Rectangle{
-                .x = @floatFromInt((p.x - @divTrunc(p.tex.width, 2))),
-                .y = @floatFromInt((p.y - @divTrunc(GAP, 2)) - p.tex.height),
-                .width = @floatFromInt(p.tex.width),
-                .height = @floatFromInt(p.tex.height),
             },
-            rl.Vector2{
-                .x = 0.0,
-                .y = 0.0
-            },
+            // rl.Rectangle{
+            //     .x = @floatFromInt((p.x - @divTrunc(p.tex.width, 2))),
+            //     .y = @floatFromInt((p.y - @divTrunc(GAP, 2)) - p.tex.height),
+            //     .width = @floatFromInt(p.tex.width),
+            //     .height = @floatFromInt(p.tex.height),
+            // },
+            p.top_rect,
+            rl.Vector2{ .x = 0.0, .y = 0.0 },
             0.0,
             rl.Color.white,
         );
 
-        rl.drawTexture(
+        // rl.drawTexture(
+        //     p.tex.*,
+        //     (p.x - @divTrunc(p.tex.width, 2)),
+        //     (p.y + @divTrunc(GAP, 2)),
+        //     rl.Color.white,
+        // );
+
+        rl.drawTextureRec(
             p.tex.*,
-            (p.x - @divTrunc(p.tex.width, 2)),
-            (p.y + @divTrunc(GAP, 2)),
+            rl.Rectangle{
+                .x = 0.0,
+                .y = 0.0,
+                .width = @floatFromInt(p.tex.width),
+                .height = @floatFromInt(p.tex.height),
+            },
+            rl.Vector2{ .x = p.bot_rect.x, .y = p.bot_rect.y },
             rl.Color.white,
         );
     }
-    
+
     pub fn update(p: *Pipe) void {
-        p.x += p.vel_x;
+        // p.x += p.vel_x;
+        p.top_rect.x += @floatFromInt(p.vel_x);
+        p.bot_rect.x += @floatFromInt(p.vel_x);
     }
 };
